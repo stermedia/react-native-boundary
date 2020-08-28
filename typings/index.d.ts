0deaa3cdcf825b52d961215c53fd2693c8e21b34
Marcin Olek <marcin@spotio.com>
2020-08-28 17:22:34 +0200

    add event onLog to .ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,23 +1,27 @@
-import {EmitterSubscription} from 'react-native';
+import { EmitterSubscription } from "react-native";
 
 export enum Events {
-    EXIT = 'onExit',
-    ENTER = 'onEnter',
+  EXIT = "onExit",
+  ENTER = "onEnter",
+  LOG = "onLog",
 }
 
 export interface Boundary {
-    id: string;
-    lat: number;
-    lng: number;
-    radius: number;
+  id: string;
+  lat: number;
+  lng: number;
+  radius: number;
 }
 
 export interface BoundaryStatic {
-    on: (event: Events, callback: (boundaries: string[]) => void) => EmitterSubscription;
-    off: (event: Events) => void;
-    add: (boundary: Boundary) => Promise<string>;
-    remove: (id: string) => Promise<null>;
-    removeAll: () => Promise<null>;
+  on: (
+    event: Events,
+    callback: (boundaries: string[]) => void
+  ) => EmitterSubscription;
+  off: (event: Events) => void;
+  add: (boundary: Boundary) => Promise<string>;
+  remove: (id: string) => Promise<null>;
+  removeAll: () => Promise<null>;
 }
 
 declare let Boundary: BoundaryStatic;
